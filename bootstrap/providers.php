a06edae55d31b49946804dea92aa7fd90c213e9a
Muhammad Idrus Alawi <muhamadidrus037@gmail.com>
2026-06-17 16:57:13 +0700

    fix: https deploy

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -9,6 +9,7 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        URL::forceRootUrl(config('app.url'));
         URL::forceScheme('https');
     }
 }
